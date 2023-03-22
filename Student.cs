ece738f3215f73274704afa7d4882112901b3bb9
tvjj10 <tvjj10@gmail.com>
2023-03-21 20:22:02 -0400

    Changed birthDate property

--- Student.cs
+++ Student.cs
@@ -12,8 +12,15 @@ class Student{
 
     private DateTime _birthDate; // use underscore for naming private fields
     public DateTime birthDate{ //same name without underscore
-        get;// do we want to make this visible
-        set; //we'll add validation
+        //get;// do we want to make this visible
+        set{//we'll add validation
+            if (value < DateTime.Now.AddYears(-100)){
+                throw new ArgumentException("thats an incorrect value enter something within the last 100 years");
+            }
+            else {
+                this._birthDate = value;
+            }
+        } 
     }
     private float _average;
     public float average{
