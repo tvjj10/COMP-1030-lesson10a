b1d9ef7a56c42db335aaaa99c0feaae176311ac1
tvjj10 <tvjj10@gmail.com>
2023-03-21 20:28:41 -0400

    changed average property

--- Student.cs
+++ Student.cs
@@ -14,6 +14,7 @@ class Student{
     public DateTime birthDate{ //same name without underscore
         //get;// do we want to make this visible
         set{//we'll add validation
+            // use value keyword to access the new value being set
             if (value < DateTime.Now.AddYears(-100)){
                 throw new ArgumentException("thats an incorrect value enter something within the last 100 years");
             }
@@ -24,8 +25,18 @@ class Student{
     }
     private float _average;
     public float average{
-        get;// read operation
-        set; //write operation
+        get{
+            return this._average; //just return whatever value is stored
+        }// read operation
+        set{ //write operation
+            //add valid averages from 0-100
+            if (value >= 0 && value <=100){
+                this._average = value;
+            }
+            else{
+                this._average = 0; //no exception, hangle invalid range by setting default value.
+            }
+        } 
     }
     //constructors
     //public methods
